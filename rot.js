1cceb2fe0d5baafd8888fec115b3836109e249c3
guy <shaun@shitchell.com>
2020-07-05 19:06:38 -0400

    removed jQuery dependency

--- rot.js
+++ rot.js
@@ -1,29 +1,25 @@
-var email_username = "fnaql";
-var email_domain = "fnaqlnyv.pbz";
-
 function rot13(str) {
-	var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-	var index     = x => input.indexOf(x);
-	var translate = x => index(x) > -1 ? output[index(x)] : x;
-	return str.split('').map(translate).join('');
+    var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    var index     = x => input.indexOf(x);
+    var translate = x => index(x) > -1 ? output[index(x)] : x;
+    return str.split('').map(translate).join('');
 }
 
 function deRotNode(node) {
-	data = $(node).data();
-	$.each(data, function(key, value) {
-		value = rot13(value);
-		if (key === "inner") {
-			$(node).html(value);
-		} else {
-			$(node).attr(key, value);
-		}
-	});
+    for (let key in node.dataset) {
+        value = rot13(node.dataset[key]);
+        if (key === "inner") {
+            node.innerText = value;
+        } else {
+            node.setAttribute(key, value);
+        }
+    }
 }
 
-$(document).ready(function() {
-	// Convert all rot13 nodes
-	$.each($(".rot13"), function(i, el) {
-		deRotNode(el);
-	})
+document.addEventListener("DOMContentLoaded", function() {
+    var nodes = document.getElementsByClassName("rot13");
+    for (let node of nodes) {
+        deRotNode(node);
+    }
 });
